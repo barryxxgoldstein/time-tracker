8e01cc0de89219b05e854922356c40acd8755586
barryxxgoldstein <barry.xx.goldstein@ericsson.com>
2018-04-23 11:16:29 +0000

    some improvements

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -16,7 +16,6 @@ public class Tracker {
     }
 
     public void remove(TimeEntry entry) {
-        if (true)
             entries.remove(entry);
 
         entries.remove(entry);
@@ -27,15 +26,12 @@ public class Tracker {
     }
 
     public TimeEntry get(int index) {
-        try {
-
-        } catch (Exception e) {
-
+       
         }
 
         boolean valid = false;
 
-        if (valid = true) {
+        if (valid == true) {
             // whatever
         }
 
